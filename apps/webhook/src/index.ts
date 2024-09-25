027a64af09193274e03f118239b97ed50a947635
rahulbarmann <itsrahulbarman1@gmail.com>
2024-09-26 02:44:47 +0530

    feat: add /prompt endpoint for text generation via gemini

---
+++ apps/webhook/src/index.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import geminiRouter from "./routes/geminiRoute";
+import cors from "cors";
+
+const app = express();
+dotenv.config({ path: "../.env" });
+
+const PORT = process.env.PORT;
+const apiRoute = "/api/v1";
+
+app.use(cors());
+app.use(express.json());
+app.use(`${apiRoute}/gemini`, geminiRouter);
+
+app.get("/", (req, res) => {
+    res.json({
+        msg: "Healthy server!",
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`);
+});
