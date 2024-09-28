1669e450e2d4034edec30bb7b97007bb9ddb48df
Rudra-Sankha-Sinhamahapatra <rssmp120@gmail.com>
2024-09-28 23:36:45 +0530

    ui changes

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,16 @@
 import Image from "next/image";
 import { Hero } from "./components/Hero";
+import { Features } from "./components/Features";
+import { HowItWorks } from "./components/HowItWorks/HowItWorks";
+import { FAQ } from "./components/FAQ/FAQ";
 
 export default function Home() {
   return (
-    <div className="flex flex-col w-full h-screen overflow-hidden">
+    <div className="flex flex-col w-full min-h-screen overflow-hidden">
    <Hero/>
+   <Features/>
+   <HowItWorks/>
+   <FAQ/>
     </div>
   );
 }
