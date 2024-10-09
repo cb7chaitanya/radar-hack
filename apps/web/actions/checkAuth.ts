aa55a8ecd7e0d7f312dd3c6961f60fff29d06873
Supriya Gorai <supriyagorai1995@gmail.com>
2024-10-09 12:18:57 +0530

    fix next redirect issue

--- apps/web/actions/checkAuth.ts
+++ apps/web/actions/checkAuth.ts
@@ -3,17 +3,18 @@
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import { isRedirectError } from "next/dist/client/components/redirect";
 
 export async function checkAuth() {
   try {
-
     const session = await getServerSession(authOptions);
-    
+
     if (!session?.user) {
       redirect("/api/auth/signin");
     }
     return session;
   } catch (error) {
-    throw new Error(`Failed to get session:${error} `);
+    if (isRedirectError(error)) throw error;
+    // redirect("/");
   }
 }
--- apps/web/app/chat/page.tsx
+++ apps/web/app/chat/page.tsx
@@ -6,9 +6,10 @@ export const dynamic = "force-dynamic";
 export default async function Page() {
   const session: any = await checkAuth();
   const userId = session.user.id;
+
   return (
     <div className="flex flex-col min-h-screen dark:bg-gradient-to-r dark:from-primary-900 dark:via-primary-800  dark:to-gray-800 text-gray-900 dark:text-gray-100 transition-colors duration-500">
-        <Chat userId={userId} />
+      <Chat userId={userId} />
     </div>
   );
 }
